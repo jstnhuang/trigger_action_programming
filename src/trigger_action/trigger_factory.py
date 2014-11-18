1a5c25431d687e9db894f5ae6631c70e5ef164e0
Justin Huang <jstn@cs.washington.edu>
2014-11-17 20:35:22 -0800

    Time of day trigger doesn't use a time provider anymore.

--- src/trigger_action/trigger_factory.py
+++ src/trigger_action/trigger_factory.py
@@ -14,17 +14,12 @@ def build(trigger_name, trigger_params, is_mock=False):
             trigger = person_detected.PersonDetected()
             return trigger
     elif trigger_name == 'time_of_day':
-        time_provider = None
-        if is_mock:
-            time_provider = time_of_day.MockTimeProvider()
-        else:
-            time_provider = time_of_day.RosTimeProvider()
         if 'hour' not in params or 'minute' not in params or 'days' not in params:
             raise ValueError('"hour", "minute", and/or "days" parameter(s) missing.')
         hour = int(params['hour'])
         minute = int(params['minute'])
         days = set(params['days'])
-        trigger = time_of_day.TimeOfDay(time_provider, hour, minute, days)
+        trigger = time_of_day.TimeOfDay(hour, minute, days)
         return trigger
     else:
         raise NotImplementedError
