f637d61ca8b0431af6ba2217e963d0556473993a
Justin Huang <jstn@cs.washington.edu>
2014-12-09 11:54:25 -0800

    Add a database-only node implementation.

--- src/trigger_action/node_factory.py
+++ src/trigger_action/node_factory.py
@@ -2,6 +2,9 @@
 import database_factory
 import node
 
-def build(name, is_mock=False):
+def build(name, is_mock):
     db = database_factory.build(is_mock)
-    return node.Node(name, db, is_mock)
+    if is_mock:
+        return node.Node(name, db, is_mock)
+    else:
+        return node.DatabaseOnlyNode(name, db, is_mock)
