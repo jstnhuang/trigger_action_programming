bc85f89fe80d5f6065e086c96c5b7bca85e5e8be
Justin Huang <jstn@cs.washington.edu>
2014-12-09 16:27:29 -0800

    Delete commented out code.

--- dart/trigger_action/web/statement.dart
+++ dart/trigger_action/web/statement.dart
@@ -7,28 +7,14 @@ import 'package:polymer/polymer.dart';
 class StatementElement extends PolymerElement {
   @published Statement model;
   @observable List<Trigger> triggers = toObservable([]);
-//  @published String id = 'unknown';
-////  @published @observable List<TriggerModel> triggers = toObservable([]);
-//  @published String action_name = '';
-//  @published Map<String, String> action_params = {};
-//  @published bool isNew = false;
   @observable String saveLabel = '';
   @observable String deleteLabel = '';
-//  @published List<StatementModel> parentList;
   @observable bool opened = false;
-
-//  @published Ros ros;
-//  Service _addStatementClient;
-//  Service _updateStatementClient;
-//  Service _deleteStatementClient;
 
   StatementElement.created() : super.created() {
   }
 
   void attached() {
-//    this._addStatementClient = new Service(this.ros, '/add_rule', 'trigger_action_programming/AddRule');
-//    this._updateStatementClient = new Service(this.ros, '/update_rule', 'trigger_action_programming/UpdateRule');
-//    this._deleteStatementClient = new Service(this.ros, '/delete_rule', 'trigger_action_programming/DeleteRule');
     updateButtons();
     triggers = toObservable(model.triggers);
     Timer timer = new Timer(new Duration(milliseconds: 50), () {
@@ -39,8 +25,6 @@ class StatementElement extends PolymerElement {
       triggers.add(trigger);
     }
   }
-
-  
 
   void updateButtons() {
     if (model.isNew) {
