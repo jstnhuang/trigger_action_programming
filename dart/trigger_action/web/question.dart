40d1373949f823d4d1183b186704ed10f9fc00b0
Justin Huang <jstn@cs.washington.edu>
2015-01-27 11:14:51 -0800

    Created a question element.

---
+++ dart/trigger_action/web/question.dart
@@ -0,0 +1,7 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('webstudy-question')
+class WebstudyQuestionElement extends PolymerElement {
+  WebstudyQuestionElement.created() : super.created() {
+  }
+}
