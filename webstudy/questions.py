2bcb0a96f0435db37860c207a6bb7b0c91f440d3
Justin Huang <jstn@cs.washington.edu>
2015-01-29 11:32:40 -0800

    Fix syntax.

--- webstudy/questions.py
+++ webstudy/questions.py
@@ -2,79 +2,54 @@
 # Question text separated by newlines will automatically become paragraphs.
 DEFAULT = [
     {
-        'question': {
-            'type': 'synthesis',
-            'text': (
-                'Suppose you want your robot to wake you up at 8:00 AM every'
-                ' day.'
-                '\nAdd one or more rules to program this behavior.'
-                ' When you are done, click "Next question."'
-            )
-        },
-        'response': {
-            'type': 'next'
-        }
+        'type': 'synthesis',
+        'text': (
+            'Suppose you want your robot to wake you up at 8:00 AM every'
+            ' day.'
+            '\nAdd one or more rules to program this behavior.'
+            ' When you are done, click "Next question."'
+        )
     },
     {
-        'question': {
-            'type': 'understanding',
-            'text': (
-                'What does this program do?'
-                '\nChoose the answer that best explains what the program does.'
-            )
-            'rules': '[]'
-        }
-        'response': {
-            'type': 'multiple_choice',
-            'options': [
-                'Greets a person at 3:00 PM',
-                'Greets a person any time between 3:00 - 4:00 PM',
-                'Says "hello" any time between 3:00 - 4:00 PM',
-                'Says "hello" at 3:00 PM'
-            ]
-        }
-    },
-    {
-        'question': (
-            'type': 'synthesis',
-            'text': (
-                'Suppose you work on weekdays between 9:00 AM and 5:00 PM. You'
-                ' want your robot to send you an email notification whenever'
-                ' someone is in the house at that time.'
-                '\nAdd one or more rules to program this behavior.'
-                ' When you are done, click "Next question."'
-            )
+        'type': 'understanding',
+        'text': (
+            'What does this program do?'
+            '\nChoose the answer that best explains what the program does.'
         ),
-        'response': {
-            'type': 'next'
-        }
+        'rules': '[]',
+        'options': [
+            'Greets a person at 3:00 PM',
+            'Greets a person any time between 3:00 - 4:00 PM',
+            'Says "hello" any time between 3:00 - 4:00 PM',
+            'Says "hello" at 3:00 PM'
+        ]
     },
     {
-        'question': (
-            'type': 'synthesis',
-            'text': (
-                'Suppose you want the lights to be on between 6:00 PM - 10:00'
-                ' PM every day.'
-                '\nAdd one or more rules to program this behavior.'
-                ' When you are done, click "Next question."'
-            )
-        ),
-        'response': {
-            'type': 'next'
-        }
+        'type': 'synthesis',
+        'text': (
+            'Suppose you work on weekdays between 9:00 AM and 5:00 PM. You'
+            ' want your robot to send you an email notification whenever'
+            ' someone is in the house at that time.'
+            '\nAdd one or more rules to program this behavior.'
+            ' When you are done, click "Next question."'
+        )
     },
     {
-        'question': (
-            'type': 'synthesis',
-            'text': (
-                'Suppose you are expecting a guest to arrive at 3:00 PM. You'
-                ' want your robot to open the door when the doorbell rings.'
-                '\nAdd one or more rules to program this behavior.'
-                ' When you are done, click "Next question."'
-            )
-        ),
-        'response': {
-            'type': 'next'
-        }
+        'type': 'synthesis',
+        'text': (
+            'Suppose you want the lights to be on between 6:00 PM - 10:00'
+            ' PM every day.'
+            '\nAdd one or more rules to program this behavior.'
+            ' When you are done, click "Next question."'
+        )
+    },
+    {
+        'type': 'synthesis',
+        'text': (
+            'Suppose you are expecting a guest to arrive at 3:00 PM. You'
+            ' want your robot to open the door when the doorbell rings.'
+            '\nAdd one or more rules to program this behavior.'
+            ' When you are done, click "Next question."'
+        )
     }
 ]
