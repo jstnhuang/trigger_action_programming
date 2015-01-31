108c8b4e7adcf7ee8375787cc6d8c80ff9083f47
Justin Huang <jstn@cs.washington.edu>
2015-01-30 21:18:05 -0800

    Added server-side error checking and completion code.

--- webstudy/main.py
+++ webstudy/main.py
@@ -20,14 +20,41 @@ def experiment_key(experiment_id):
     return ndb.Key('Experiment', experiment_id)
 
 
+class Participant(ndb.Model):
+    completion_code = ndb.IntegerProperty()
+
+
 class Response(ndb.Model):
-    participant_id = ndb.IntegerProperty()
+    participant_key = ndb.KeyProperty()
     question_id = ndb.IntegerProperty()
     rules = ndb.JsonProperty()
     selected = ndb.StringProperty()
 
+    @classmethod
+    def has_entry(cls, exp_id, participant_key, qid):
+        count = cls.query(
+            cls.participant_key == participant_key,
+            cls.question_id == qid,
+            ancestor=experiment_key(exp_id)
+        ).count(limit=1)
+        return count > 0
+
+    @classmethod
+    def check_responses_complete(cls, exp_id, participant_key):
+        """Returns whether the participant has completed the experiment.
+        """
+        count = cls.query(
+            cls.participant_key == participant_key,
+            ancestor=experiment_key(exp_id)
+        ).count()
+        return count == len(questions.DEFAULT)
+
 
 def allow_localhost(f):
+    """Allows cross-origin requests from localhost:8080.
+
+    Used for debugging the Dart app, which pub serves from that origin.
+    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         response = make_response(f(*args, **kwargs))
@@ -42,12 +69,12 @@ def home():
     return render_template('start.html')
 
 
-@app.route('/question/<int:participant_id>/<int:question_id>')
-def question(participant_id, question_id):
-    response = make_response(json.dumps(questions.DEFAULT[question_id]))
-    response.headers['Access-Control-Allow-Origin'] = (
-        'http://localhost:8080')
-    return response
+@app.route('/question/<participant_key>/<int:question_id>')
+@allow_localhost
+def question(participant_key, question_id):
+    if question_id < 0 or question_id > len(questions.DEFAULT) - 1:
+        return redirect(url_for('error'))  # Unknown question
+    return json.dumps(questions.DEFAULT[question_id])
 
 
 @app.route('/next', methods=['POST'])
@@ -55,40 +82,64 @@ def question(participant_id, question_id):
 def next():
     try:
         data = json.loads(request.data)
-        participant_id = int(data['p'])
+        participant_key = ndb.Key(urlsafe=data['p'])
+        participant = participant_key.get()
+        if participant is None:
+            return url_for('error')  # Unknown participant
         question_id = int(data['q'])
+        if question_id < 0 or question_id > len(questions.DEFAULT) - 1:
+            return url_for('error')  # Unknown question
         rules = data['rules'] if 'rules' in data else ''
         selected = data['selected'] if 'selected' in data else ''
-        response = Response(parent=experiment_key(DEFAULT_EXPERIMENT))
-        response.participant_id = participant_id
-        response.question_id = question_id
-        response.rules = rules
-        response.selected = selected
-        response.put()
+
+        # Don't insert duplicate responses.
+        if not Response.has_entry(
+                DEFAULT_EXPERIMENT, participant_key, question_id):
+            response = Response(parent=experiment_key(DEFAULT_EXPERIMENT))
+            response.participant_key = participant_key
+            response.question_id = question_id
+            response.rules = rules
+            response.selected = selected
+            response.put()
 
         if question_id == len(questions.DEFAULT) - 1:
-            # TODO: Check if all questions have actually been answered. If not,
-            # redirect to an error page.
-            return url_for('end')
+            complete = Response.check_responses_complete(
+                DEFAULT_EXPERIMENT, participant_key)
+            if complete:
+                code = None
+                if participant.completion_code is not None:
+                    code = participant.completion_code
+                else:
+                    code = random.randint(0, 1000000)
+                    participant.completion_code = code
+                    participant.put()
+                return url_for('end', code=code)
+            else:
+                return url_for('error')
 
-        question_id += 1
         return '/txg/webstudy.html?p={}&q={}'.format(
-            participant_id, question_id)
+            participant_key.urlsafe(), question_id + 1)
     except ValueError:
-        return url_for('error')
+        return url_for(
+            'error', msg='The wrong data was sent to the application.')
+    except KeyError:
+        return url_for('error', msg='The response was incomplete.')
+    except Exception as e:
+        return url_for('error', msg='An unknown error occurred.')
 
 
 @app.route('/start')
 def start():
-    participant_id = random.randint(0, 100000)
-    return redirect('/txg/webstudy.html?p={}&q=0'.format(participant_id))
+    participant = Participant(parent=experiment_key(DEFAULT_EXPERIMENT))
+    key = participant.put()
+    return redirect('/txg/webstudy.html?p={}&q=0'.format(key.urlsafe()))
 
 
-@app.route('/end')
-def end():
-    return render_template('end.html')
+@app.route('/end/<code>')
+def end(code):
+    return render_template('end.html', code=code)
 
 
-@app.route('/error')
-def error():
-    return render_template('error.html')
+@app.route('/error/<msg>')
+def error(msg):
+    return render_template('error.html', msg=msg)
